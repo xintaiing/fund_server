9df34e8bb309b7cc6f51fd70ffec6e2bc1419ee4
sxt <sxt@zionreality.com>
2020-11-12 11:56:40 +0800

    配置线上数据库

--- fund_server/__init__.py
+++ fund_server/__init__.py
@@ -1,11 +1,11 @@
 # 日志名
 LOGGER_NAME = "dev"
 
-LOCAL_HOST = "127.0.0.1"
+LOCAL_HOST = "139.196.212.119"
 
 # db conn config
 DB_NAME = "fund_server"
 DB_USER = "root"
-DB_PASSWORD = "123123"
+DB_PASSWORD = "FS.2020.11.11.sxt"
 DB_HOST = LOCAL_HOST
 DB_PORT = "3306"
