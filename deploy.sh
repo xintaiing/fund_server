6dfe107a72acbedb4cd4d5eca734230484797128
xintaiing <45135426+xintaiing@users.noreply.github.com>
2022-10-04 22:33:06 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,5 @@
-cd ..
-tar -zcvf fund_server.tar.gz fund_server
-scp -i ~/.ssh/aliyun_ubuntu.pem fund_server.tar.gz ubuntu@139.196.212.119:~/workspace/project
-ssh -i ~/.ssh/aliyun_ubuntu.pem ubuntu@139.196.212.119 << EOF
-
-cd /home/ubuntu/workspace/project
-tar -zxvf fund_server.tar.gz
-mv fund_server/docker-compose.yml .
+#!/bin/bash
 docker-compose stop
 yes | docker-compose rm
-docker rmi project_web:latest
+docker rmi fund_server_web:latest
 docker-compose up -d
-
-EOF
